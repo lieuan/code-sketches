2fbd316cea100de104dd6cc19ffdbb6170b88ff1
lieuan <lieuab8@gmail.com>
2026-01-27 14:00:41 +0100

    Mettre un sketch sur la page

---
+++ circles.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+var size = 10;
+
+function draw() {
+  noFill();
+  circle(200,200,size);
+  size += 10; //size = size plus 10
+  stroke(255, 0, 0);
+  
+  
+}                       
